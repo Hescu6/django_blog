cafa237bef625909610c2498c7ab6ce3a309ab33
Hil <hildebrando.escudero@udc.edu>
2020-04-27 11:35:21 -0400

    user app and registration created

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'Hilde',
-        'title': 'blog post 1',
-        'content': 'Firstpost hardcoded',
-        'date_posted': 'April 23, 2020'
-    },
-    {
-        'author': 'Hildebra',
-        'title': 'blog post 2',
-        'content': 'Secondpost hardcoded',
-        'date_posted': 'April 24, 2020'
-    }
-
-]
 
 def home(request):
     context = {
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
