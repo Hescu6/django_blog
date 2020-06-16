da33dbbac76fe19116a23325f4469c9f1998074a
Hil <hildebrando.escudero@udc.edu>
2020-06-16 13:20:23 -0400

    added routing for individual posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
+
 
 
 def home(request):
@@ -7,6 +9,18 @@ def home(request):
         'posts':Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
