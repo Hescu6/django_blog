0512944ef97ed7c090687513b37c5ef5d6d3489c
Hildebrando <tususpop@gmail.com>
2021-08-05 23:11:21 -0400

    added secret keys and requirements

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -25,13 +25,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config['SECRET_KEY']
+SECRET_KEY = config.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config['DEBUG']
+DEBUG = config.get('DEBUG')
 
-ALLOWED_HOSTS = [config['PUBLIC_IP']]
+#array
+ALLOWED_HOSTS = config.get('PUBLIC_IP')
 
 
 # Application definition
@@ -144,5 +145,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 #EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 #EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
-EMAIL_HOST_USER = config['EMAIL_USER']
-EMAIL_HOST_PASSWORD = config['EMAIL_PASS']
+EMAIL_HOST_USER = config.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = config.get('EMAIL_PASS')
